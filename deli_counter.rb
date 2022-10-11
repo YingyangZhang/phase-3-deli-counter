f30b1685f4c1aa01f94699558350de07fb07d895
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-10-11 12:56:24 -0400

    commit tree 534eacb117a0dab06cacabb4487032e96fdd9199

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line arr
+    if arr.empty? 
+        puts "The line is currently empty."
+    else
+        currentLine = "The line is currently:"
+        arr.each.with_index(1) do |element, index|           
+            currentLine << " #{index}. #{element}"
+        end
+        puts currentLine
+    end
+end
+
+def take_a_number (arr, name)
+    arr << name
+    puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
+
+def now_serving arr
+    if arr.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{arr.first}."
+        arr.shift
+    end
+end
